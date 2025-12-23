38b8524ac19abf4b0332559ba3d677c5af292b6a
Marpp98 <mar.p.p98@gmail.com>
2025-12-23 13:00:34 +0100

    actu datos, creacion datos habitacion y canales

--- 02_data.sql
+++ 02_data.sql
@@ -6,10 +6,19 @@ INSERT INTO servicios (id_servicio, tipo_servicio) VALUES
 (3, 'Spa'),
 (4, 'Excusriones');
 
-LOAD DATA LOCAL INFILE '"C:\Users\maria\Desktop\Master_data_science\SQL_Resort_Relational_Database_EDA\data\clientes.csv"'
-INTO TABLE clientes
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(id_cliente, nombre, apellidos, email, telefono, pais);
+-- Los datos de la tabla clientes han sido importados mediante table Data Import Wizard desde clientes.csv
+
+select * from servicios;
+SELECT * FROM clientes;
+select * from habitaciones;
+
+INSERT INTO canales (id_canal, canal_distribucion) VALUES
+(1, 'Web_Propia'),
+(2, 'Call_Center/Recepcion'),
+(3, 'Agencia_Viajes_Online'),
+(4, 'Agencia_Viajes_Offline'),
+(5, 'Corporativo/GDS'),
+(6, 'Touroperadores'),
+(7, 'MICE_Grupos_Eventos');
+
+select * from canales;
