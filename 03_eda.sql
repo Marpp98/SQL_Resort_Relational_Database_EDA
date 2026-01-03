ad7ef20517611c067a043831a0970266f47252f3
Marpp98 <mar.p.p98@gmail.com>
2026-01-03 20:56:20 +0100

    creacion de querys

--- 03_eda.sql
+++ 03_eda.sql
@@ -80,4 +80,143 @@ GROUP BY tipo_comida
 ORDER BY contrataciones DESC;
  
  
- select * from servicio_comida;
+ /*===============================================
+	6. Qué servicio de spa es el más contratado.
+ =================================================*/ 
+ -- El servicio más contratado es balneario.
+ SELECT 
+    tipo_tratamiento, 
+    COUNT(*) AS contrataciones
+FROM
+    servicio_spa
+GROUP BY tipo_tratamiento
+ORDER BY contrataciones DESC;
+ 
+ 
+ /*===============================================
+	7. ¿Cuál es la media de plazas de parking contratadas?¿Y la moda?
+ =================================================*/ 
+ -- Media = 1
+SELECT 
+    ROUND(AVG(numero_plazas)) AS media_plazas
+FROM
+    servicio_parking;
+    
+ -- Moda: 1 plaza moda = 5600
+ 
+SELECT 
+	numero_plazas AS moda_plazas,
+    COUNT(*) AS frecuencia
+FROM 
+	servicio_parking
+GROUP BY 
+	moda_plazas
+ORDER BY 
+	frecuencia DESC
+LIMIT 1;
+
+
+ /*===============================================
+	8. ¿Qué tipo de habitaciones se han contratado más?
+ =================================================*/ 
+-- La habitación más contratada es la doble interior.
+SELECT 
+	h.tipo, 
+	COUNT(r.id_reserva) AS reservas
+FROM 
+	reservas r
+JOIN habitaciones h ON h.id_habitacion = r.id_habitacion
+GROUP BY h.tipo
+ORDER BY reservas DESC;
+
+ /*===============================================
+	9. ¿Cuál es la estancia media, la máxima y la mínima?
+ =================================================*/ 
+-- Media = 3
+-- Max = 60
+-- Min = 0
+
+SELECT 
+	ROUND(AVG(DATEDIFF(checkout, checkin))) AS estancia_media,
+    MAX(DATEDIFF(checkout, checkin)) AS estancia_maxima,
+    MIN(DATEDIFF(checkout, checkin)) AS estancia_minima
+FROM reservas
+WHERE estado_reserva = 'Check-Out';
+
+select * from reservas;
+
+ /*===============================================
+	10. Clasifica a los clientes en plata, oro y diamante según la cantidad de veces que hayan visitado el hotel. 
+		**Uso de CASE/WHEN
+ =================================================*/
+SELECT 
+    c.id_cliente,
+    c.nombre,
+    c.apellidos,
+    COUNT(r.id_reserva) AS numero_visitas,
+    CASE 
+        WHEN COUNT(r.id_reserva) >= 30 THEN 'Diamante'
+        WHEN COUNT(r.id_reserva) >= 20 THEN 'Oro'
+        ELSE 'Plata'
+    END AS categoria_fidelidad
+FROM 
+    clientes c
+JOIN 
+    reservas r ON c.id_cliente = r.id_cliente
+WHERE 
+    r.estado_reserva = 'Check-Out' -- Solo contamos visitas completadas
+GROUP BY 
+    c.id_cliente
+ORDER BY 
+    numero_visitas DESC;
+    
+    
+ /*===============================================
+	11. Cuántos clientes hay de cada categoría de  clientes?
+		**Uso de desubconsulta.
+ =================================================*/
+SELECT 
+    categoria_fidelidad, 
+    COUNT(*) AS total_clientes
+FROM (
+    SELECT 
+        c.id_cliente,
+        CASE 
+            WHEN COUNT(r.id_reserva) >= 40 THEN 'Diamante'
+            WHEN COUNT(r.id_reserva) >= 20 THEN 'Oro'
+            ELSE 'Plata'
+        END AS categoria_fidelidad
+    FROM 
+        clientes c
+    JOIN 
+        reservas r ON c.id_cliente = r.id_cliente
+    WHERE 
+        r.estado_reserva = 'Check-Out'
+    GROUP BY 
+        c.id_cliente
+) AS resumen_clientes
+GROUP BY 
+    categoria_fidelidad
+ORDER BY 
+    total_clientes DESC;
+    
+    
+ /*===============================================
+	11. Funcion que indica el cálculo total para cada reserva
+ =================================================*/ 
+
+SELECT 
+    r.id_reserva, 
+    DATEDIFF(r.checkout, r.checkin) AS estancia,
+    r.deposito, 
+    p.precio_total AS coste_parking, 
+    h.tarifa AS tarifa_habitacion, 
+    c.precio AS tarifa_comida,
+    s.precio AS tarifa_spa,
+    (DATEDIFF(r.checkout, r.checkin) * h.tarifa) + (DATEDIFF(r.checkout, r.checkin) * p.precio_total)AS COSTE_TOTAL
+FROM
+    reservas r
+JOIN servicio_parking p ON r.id_reserva = p.id_reserva
+JOIN habitaciones h ON h.id_habitacion = r.id_habitacion
+JOIN servicio_comida c ON r.id_reserva = c.id_reserva
+JOIN servicio_spa s ON r.id_reserva = s.id_reserva;
